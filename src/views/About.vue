9ca13d59838995820d1e8cc2d5d888d1c9dc86da
hgozutok <hgozutok@gmail.com>
2022-02-09 14:11:17 +0000

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container-fluid text-center text-white bg-dark w-100 h-100">
+    <div class="container-fluid d-flex p-3 mx-auto flex-column">
+      <Navbar />
+      <div class="about">
+        <h1>This is an about page</h1>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<script>
+import Navbar from "@/components/nav/Navbar.vue";
+export default {
+  name: "About",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+</style>
