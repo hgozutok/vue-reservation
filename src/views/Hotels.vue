60a69016f72c65dfe62b5b53290cb724d26a066d
Huseyin GOZUTOK <hgozutok@gmail.com>
2022-03-14 12:57:30 +0000

    hotelroom

--- src/views/Hotels.vue
+++ src/views/Hotels.vue
@@ -32,19 +32,74 @@
       <!-- hotel images and map -->
       <div v-if="pageLoaded === true" class="row">
         <div class="col-md-8">
-          <Carousel :value="hotelImages" :numVisible="1" :numScroll="1">
-            <template #header>
-              <h2>Hotel Images</h2>
-            </template>
-            <template #item="slotProps">
-              <div>
-                <img :src="slotProps.data.brand" :alt="slotProps.data.brand" />
+          <div class="card responsive">
+            <Carousel :value="hotelImages" :numVisible="1" :numScroll="1">
+              <template #header>
+                <h5>Hotel Images</h5>
+              </template>
+              <template #item="slotProps">
+                <div class="product-item responsive">
+                  <div class="product-item-content">
+                    <div class="mb-3">
+                      <img
+                        v-bind:src="
+                          slotProps.data.baseUrl.replace(
+                            '{size}',
+                            slotProps.data.sizes[1].suffix
+                          )
+                        "
+                        :alt="slotProps.data.baseUrl"
+                        class="product-image responsive"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </Carousel>
+          </div>
+          <div v-if="pageLoaded === true" class="row">
+            <div class="col-md-8">
+              <h4>Rooms</h4>
+
+              <div v-bind:key="room" v-for="room in roomsAndRates.rooms">
+                <h4>{{ room.name }}</h4>
+                <h6>Description</h6>
+                {{ room.additionalInfo.description }}
+                <h6>Room Type</h6>
+                {{ room.bedChoices.mainOptions[0] }}
+                <h6>Room Type</h6>
+                {{ room.ratePlans[0].cancellation.info }}
               </div>
-            </template>
-          </Carousel>
+            </div>
+          </div>
+          <div v-if="pageLoaded === true" class="row">
+            <div class="col-md-8">
+              <h4>At Glance</h4>
+
+              <h6>Arriving:</h6>
+              <p>{{ atAGlance.keyFacts.arrivingLeaving[0] }}</p>
+              <h6>Leaving:</h6>
+              <p>{{ atAGlance.keyFacts.arrivingLeaving[1] }}</p>
+              <h6>Hotel Capacity:</h6>
+              <p>{{ atAGlance.keyFacts.hotelSize[0] }}</p>
+              <h6>required At Check-In:</h6>
+              <div
+                v-bind:key="item"
+                v-for="item in atAGlance.keyFacts.requiredAtCheckIn"
+              >
+                <p>{{ item }}</p>
+              </div>
+              <h6>Hotel Capacity:</h6>
+              <p>
+                {{ atAGlance.keyFacts.specialCheckInInstructions[0] }}
+              </p>
+            </div>
+          </div>
         </div>
+
         <div class="col-md-4">
           <div id="map" style="width: 100%; height: 400px">
+            <img v-bind:src="propertyDescription.mapWidget.staticMapUrl" />
             <iframe
               width="300"
               height="170"
@@ -58,7 +113,28 @@
             <address>
               <strong>{{ propertyDescription.address.fullAddress }}</strong>
             </address>
-            <div>{{ guestReviews.formattedRating }}</div>
+            <div>
+              Guests Review:{{ guestReviews.brands.formattedRating }}/{{
+                guestReviews.brands.formattedScale
+              }}
+            </div>
+            <div>Total {{ guestReviews.brands.total }} review</div>
+
+            <h4>Amenities</h4>
+            <div>
+              <div v-bind:key="amenity.heading" v-for="amenity in amenities">
+                <h3 key="amenity.heading">{{ amenity.heading }}</h3>
+
+                <div v-bind:key="item" v-for="item in amenity.listItems">
+                  <h5 key="item">{{ item.heading }}</h5>
+                  <div v-bind:key="subitem" v-for="subitem in item.listItems">
+                    <ul>
+                      <li>{{ subitem }}</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -106,51 +182,64 @@ export default {
     var pageInfo = ref();
     var hotelBadges = ref();
     var unavailable = ref();
-
+    var roomsAndRates = ref();
     var transportations = ref();
     var hotelImages = ref();
+
     var pageLoaded = ref(false);
 
-   // var location = ref(route.query.location);
+    // var location = ref(route.query.location);
     var departDate = ref(route.query.departDate);
     var returnDate = ref(route.query.leaveDate);
     var adults = ref(route.query.adults);
 
     const getHotelDetails = async () => {
-      console.log("htoelDetails")
-       SearchHotel.getHotelDetails(hotelid.value,departDate.value,returnDate.value,adults.value).then((response) => {
+      SearchHotel.getHotelDetails(
+        hotelid.value,
+        departDate.value,
+        returnDate.value,
+        adults.value
+      ).then((response) => {
         hotel.value = response;
-        pdpHeader.value = response.pdpHeader;
-        overview.value = response.overview;
-        hotelWelcomeRewards.value = response.hotelWelcomeRewards;
-        propertyDescription.value = response.propertyDescription;
-        guestReviews.value = response.guestReviews;
-        atAGlance.value = response.atAGlance;
-        amenities.value = response.amenities;
-        hygieneAndCleanliness.value = response.hygieneAndCleanliness;
-        smallPrint.value = response.smallPrint;
-        specialFeatures.value = response.specialFeatures;
-        miscellaneous.value = response.miscellaneous;
-        pageInfo.value = response.pageInfo;
+        console.log("htoelDetails", response);
+        pdpHeader.value = response.data.body.pdpHeader;
+        overview.value = response.data.body.overview;
+        hotelWelcomeRewards.value = response.data.body.hotelWelcomeRewards;
+        propertyDescription.value = response.data.body.propertyDescription;
+        guestReviews.value = response.data.body.guestReviews;
+        atAGlance.value = response.data.body.atAGlance;
+        amenities.value = response.data.body.amenities;
+        hygieneAndCleanliness.value = response.data.body.hygieneAndCleanliness;
+        smallPrint.value = response.data.body.smallPrint;
+        specialFeatures.value = response.data.body.specialFeatures;
+        miscellaneous.value = response.data.body.miscellaneous;
+        pageInfo.value = response.data.body.pageInfo;
         hotelBadges.value = response.hotelBadges;
         unavailable.value = response.unavailable;
+        roomsAndRates.value = response.data.body.roomsAndRates;
 
         transportations.value = response.transportations;
         console.log(hotel.value);
-      pageLoaded.value = true;
+        // pageLoaded.value = true;
         SearchHotel.getHotelImages(hotelid.value).then((response) => {
-          hotelImages.value = response;
+          console.log(response, "hotelImages");
+          // let images = [];
+          // response.hotelImages.forEach(function (item) {
+          //   images.push(item.baseUrl.replace("{size}", item.sizes[1].suffix));
+          // });
+
+          hotelImages.value = response.hotelImages;
+
+          console.log(response.hotelImages);
         });
       });
-
-    
     };
 
-  onMounted(() => {
-        console.log("page mounted");
-        getHotelDetails();
-        pageLoaded.value = true;
-      });
+    onMounted(() => {
+      console.log("page mounted");
+      getHotelDetails();
+      pageLoaded.value = true;
+    });
 
     return {
       getHotelDetails,
@@ -173,6 +262,7 @@ export default {
       transportations,
       hotelImages,
       pageLoaded,
+      roomsAndRates,
     };
   },
   name: "Hotels",
@@ -183,5 +273,27 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.p-carousel-next {
+  width: 2rem;
+  height: 2rem;
+  color: #061a2c;
+  border: 0 none;
+
+  border-radius: 50%;
+  transition: background-color 0.2s, color 0.2s, box-shadow 0.2s;
+  margin: 0.5rem;
+}
+.product-item .product-item-content {
+  border: 1px solid var(--surface-border);
+  border-radius: 3px;
+  margin: 0.3rem;
+  text-align: center;
+  padding: 2rem 0;
+}
+
+.product-item .product-image {
+  width: 50%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
 </style>
